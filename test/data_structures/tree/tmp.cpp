87116df0b4ad9d361e50a9d8a03afca0c5911af7
Mike Babb <mike7400@gmail.com>
2020-12-07 02:19:20 -0500

    tmp

--- test/data_structures/tree/tmp.cpp
+++ test/data_structures/tree/tmp.cpp
@@ -1,11 +1,12 @@
 #define FMT_HEADER_ONLY
 
-// #include "fmt/format.h"
-// #include "range/v3/view/enumerate.hpp"
-// #include "range/v3/view/iota.hpp"
+#include "fmt/format.h"
+#include "range/v3/view/enumerate.hpp"
+#include "range/v3/view/iota.hpp"
+#include "src/data_structures/list/array_list.hpp"
+#include "src/data_structures/list/sparse_list.hpp"
 #include "src/data_structures/tree/tree.hpp"
-#include "src/data_structures/utils/pyrange.hpp"
-#include "src/data_structures/utils/utils.hpp"
+#include "src/data_structures/utils/range.hpp"
 
 #include <iostream>
 #include <vector>
@@ -13,16 +14,23 @@
 int
 main()
 {
-    auto n = 100000;
-    std::vector<int> v;
-    v.reserve(n);
-    for (auto i = 0; i < n; i++) {
-        v.push_back(i);
-    }
-    auto x = bisect(v, 0);
+    using namespace ds;
+    // auto n = 100000;
+    // std::vector<int> v;
+    // v.reserve(n);
+    // for (auto i = 0; i < n; i++) {
+    //     v.push_back(i);
+    // }
+    // auto x = bisect(v, 0);
 
-    for (auto i : pyrange(n)) {
-        std::cout << "ok" << std::endl;
-    }
+    list::array_list<int> v({0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10});
+
+    auto vv = v[list::slice(1, 5)];
+
+    auto z = utils::bisect(v, 0);
+
+    auto x = v.index(10);
+    auto y = v.index(99);
+
     return 1;
 }
